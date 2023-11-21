ed099e9771c7636b60093413e0ab9736518c3034
Stephen Schuez <stephens@hey.com>
2023-11-21 22:43:56 +0100

    Add stuff to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?  
+
+  private
+
+  def configure_permitted_parameters
+    attributes = [:full_name, :email, :password, :password_confirmation, :bio]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
