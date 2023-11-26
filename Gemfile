6069428f7086b22599b168a79d8655d15829b34d
Stephen <stephens@hey.com>
2023-11-26 22:56:26 +0100

    Add new dockerfile.dev to development

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
   gem "capybara"
   gem "database_cleaner"
 end
+
+gem 'sidekiq'
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+redis_config = Rails.env.development? ? { url: "redis://redis:6379/1" } : { url: ENV["REDIS_URL"] }
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
