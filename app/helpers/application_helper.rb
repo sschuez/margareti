5215b9162fc8f907fc38025d79c0d61ddde73403
Stephen <stephens@hey.com>
2023-11-28 09:58:17 +0100

    Fix name for empty state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,8 @@ module ApplicationHelper
   def nested_dom_id(*args)
     args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
   end
+
+  def remove_underscore(word)
+    word.gsub("_", " ")
+  end
 end
