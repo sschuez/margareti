27b2dd5e9b86b6f5477d7f4aea9d3552a159aecc
Stephen <stephens@hey.com>
2023-12-06 10:20:44 +0100

    Add cloudinary image tag for photos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,2 @@
 module UsersHelper
-  def photo_for(user)
-    if user.photo.attached?
-      image_tag user.photo.variant(resize_to_limit: [500, 500]), class: "profile__photo--img avatar-large"
-    else
-      content_tag(:div, "No photo added", class: "profile__photo--img avatar-large avatar-large--empty-state")
-    end
-  end
-
-  def photo_edit_link_for(user)
-    description = user.photo.attached? ? "Edit" : "Add"
-    turbo_frame_tag "photo-form" do
-      link_to "#{description == "Add" ? "+ " : ""}#{description} photo", edit_user_photo_path(user), data: { turbo_frame: "photo-form", controller: "action-authorisations", action_authorisations_target: "user" }
-    end
-  end
 end
