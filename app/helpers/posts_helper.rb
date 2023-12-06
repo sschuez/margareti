132a2b1175ba8f17861ddcf4706b7269ce2f11f6
Stephen <stephens@hey.com>
2023-12-06 10:41:50 +0100

    Add authorisations to post btns

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,15 +2,15 @@ module PostsHelper
   def index_actions_for(post)
     html = ''.html_safe
 
-    html << content_tag(:a, class: 'sortable-handle') do
+    html << content_tag(:a, class: 'sortable-handle', data: { controller: "action-authorisations", action_authorisations_target: "user" }) do
       Icon.call("drag")
     end
   
-    html << link_to(edit_post_path(post), target: "_top") do
+    html << link_to(edit_post_path(post), target: "_top", data: { controller: "action-authorisations", action_authorisations_target: "user" }) do
       Icon.call("edit")
     end
   
-    html << button_to(post_path(post), method: :delete, data: { turbo_confirm: "Are you sure?" }, class: "button") do
+    html << button_to(post_path(post), method: :delete, data: { turbo_confirm: "Are you sure?", controller: "action-authorisations", action_authorisations_target: "user" }, class: "button") do
       Icon.call("trash")
     end
   
