0778c02752aa08e779f7fa91487c3935722d71f3
Stephen <stephens@hey.com>
2023-12-07 11:08:39 +0100

    Refactor action authorsations controller

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/helpers", under: "helpers"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
 pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.1.2/app/assets/javascripts/activestorage.esm.js"
