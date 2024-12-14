2d0d5ea0a33a332e664f1f9fb520d612d5d8c5c9
Stephen <stephens@hey.com>
2024-12-14 10:42:21 +0100

    Add shit(direction) method to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,30 +1,42 @@
 class Post < ApplicationRecord
   belongs_to :user
-  # acts_as_list add_new_at: :top, top_of_list: 0, scope: :user
   acts_as_list scope: :user
-  
+
   validates :title, :body, presence: true
-  
+
   scope :ordered, -> { order(position: :asc) }
   scope :published, -> { where(published: true) }
-    
+
   def published_icon
-    if self.published
-      Icon.call("published")
+    if published
+      Icon.call('published')
     else
-      Icon.call("unpublished")
+      Icon.call('unpublished')
     end
   end
 
   def keyword_subtitle
-    subtitle.truncate(300).split(" ").join(", ") if subtitle.present?
+    subtitle.truncate(300).split(' ').join(', ') if subtitle.present?
   end
-    
+
   def keyword_title
-    title.split(" ").join(", ")
+    title.split(' ').join(', ')
   end
-    
+
   def keywords
     keyword_subtitle || keyword_title
+  end
+
+  def shift(direction)
+    case direction
+    when 'up'
+      other_post = user.posts.where('position < ?', position).ordered.last
+      other_post&.increment_position
+      decrement_position
+    when 'down'
+      other_post = user.posts.where('position > ?', position).ordered.first
+      other_post&.decrement_position
+      increment_position
+    end
   end
 end
